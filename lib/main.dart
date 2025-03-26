f5baba2d151d054717f58568cef210032c1a520d
ANDRES_MIGUEL_RIVERA_FRANCISCO <a.22308051281295@cbtis128.edu.mx>
2025-03-26 19:39:35 +0000

    si

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:riveracurso/my_widgets/myCard.dart';
-
 import 'my_widgets/dialogo_acerca_de.dart';
+import 'my_widgets/miLista.dart';
 
-Color darkblue = Color(0xff585858);
+Color darkblue = Color(0xfff23939);
 void main() => runApp(MiTarjeta());
 
 class MiTarjeta extends StatelessWidget {
@@ -17,12 +17,13 @@ class MiTarjeta extends StatelessWidget {
       title: "Flutter Widget Tarjeta Card",
       home: Scaffold(
           appBar: AppBar(
-            title: Text("Flutter Nava"),
-            backgroundColor: Colors.black,
+            title: Text("Flutter Rivera"),
+            backgroundColor: Color(0xff4e0000),
             centerTitle: true,
           ),
           // body: LaTarjeta()),
-          body: DialogoAcercaDe()),
+          //body: DialogoAcercaDe()),
+          body: Lista()),
     );
   }
 }
